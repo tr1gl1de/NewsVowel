fc1ce81f40d80bec23d78496fed529f1db26a87e
m.kazakov <m.kazakov@sodru.com>
2023-09-13 02:54:39 +0200

    Init

---
+++ NewsVowelTask/NewsParseExtensions.cs
@@ -0,0 +1,70 @@
+﻿using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+
+namespace NewsVowelTask;
+
+public static class NewsParseExtensions
+{
+    private static string GetNewsValue(Article article, string fieldName) =>
+        fieldName switch
+        {
+            "author" => article.Author,
+            "title" => article.Title,
+            "content" => article.Content,
+            "description" => article.Description,
+            _ => article.Content
+        };
+
+    public static async Task<IList<Article>> ParseNews(string key, string theme, int pageNum, int pageSize)
+    {
+        var newsApiClient = new NewsApiClient(key);
+        var news = await newsApiClient.GetEverythingAsync(new EverythingRequest
+        {
+            Q = theme,
+            SortBy = SortBys.Popularity,
+            PageSize = pageSize,
+            Page = pageNum
+        });
+
+        return news.Articles;
+    }
+
+    public static IReadOnlyDictionary<string, string> GetVowelsFromNews(this IEnumerable<Article> news,
+        string fieldName)
+    {
+        var newsContentWithMaxVowelsWords = new Dictionary<string, string>();
+        var enVowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+        var ruVowels = new[] { 'а', 'э', 'ы','о','у','я','е','и','ё','ю'};
+        var vowels = new HashSet<char>();
+        vowels.UnionWith(enVowels);
+        vowels.UnionWith(ruVowels);
+        var delimiters = new[]
+            {
+                ' ', ',', '.', '`', ';', '/', '\'', ':', '{', '}', '[', ']', '\\', '|', '+', '(', ')', '\t',
+                '\r', '\n', '?', '!', '"', '<', '>'
+            };
+
+        var wordWithMaxVowels = string.Empty;
+        foreach (var article in news)
+        {
+            var newsValue = GetNewsValue(article, fieldName);
+            var words = newsValue.Split(delimiters, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var maxNumOfVowels = 0;
+            foreach (var word in words)
+            {
+                var numOfVowels = word.Count(symbol => vowels.Contains(symbol));
+
+                if (numOfVowels > maxNumOfVowels)
+                {
+                    maxNumOfVowels = numOfVowels;
+                    wordWithMaxVowels = word;
+                }
+            }
+            
+            newsContentWithMaxVowelsWords.Add(newsValue, wordWithMaxVowels);
+        }
+
+        return newsContentWithMaxVowelsWords;
+    }
+}
---
+++ NewsVowelTask/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CommandLine;
+using static NewsVowelTask.NewsParseExtensions;
+
+namespace NewsVowelTask;
+
+static class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var themeNewsOption = new Option<string>(
+            name: "--theme",
+            description: "Тема новости для парсинга",
+            getDefaultValue: () => "космос");
+
+        var filedNewsOption = new Option<string>(
+            name: "--field",
+            description: "Поле новости для чтения \"author, title, description, content\"",
+            getDefaultValue: () => "title");
+
+        var newsApiKeyOption = new Option<string>(
+            name: "--key",
+            description: "Ключ от API newsapi");
+
+        var pageSizeOption = new Option<int>(
+            name: "--pageSize",
+            description: "количество элементов на странице для парсинга",
+            getDefaultValue: () => 1);
+        
+        var pageNumberOption = new Option<int>(
+            name: "--pageNum",
+            description: "Номер страницы для парсинга",
+            getDefaultValue: () => 1);
+        
+        var rootCommand =
+            new RootCommand("Программа для парсинга и поиска в новостях слова с наибольшим количеством гласных");
+
+        var parseNewsCommand = new Command("parse", "Парсинг и поиск слова")
+        {
+            themeNewsOption,
+            filedNewsOption,
+            newsApiKeyOption,
+            pageSizeOption,
+            pageNumberOption
+        };
+        rootCommand.AddCommand(parseNewsCommand);
+        
+        parseNewsCommand.SetHandler(async (key, theme, fieldName, pageSize, pageNum) =>
+        {
+            var news = await ParseNews(key, theme, pageNum, pageSize);
+            var result = news.GetVowelsFromNews(fieldName);
+            PrintResult(result);
+        }, newsApiKeyOption, themeNewsOption, filedNewsOption, pageSizeOption, pageNumberOption);
+        
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    private static void PrintResult<T, T2>(IReadOnlyDictionary<T, T2> res)
+    {
+        foreach (var keyValuePair in res)
+        {
+            Console.WriteLine($"Key:[{keyValuePair.Key}] Value:[{keyValuePair.Value}]");
+        }
+    }
+}
